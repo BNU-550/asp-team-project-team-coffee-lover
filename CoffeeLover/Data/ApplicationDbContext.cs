27d4ea880a25612281478c60269d3f97188297d1
Densai2 <61013618+Densai2@users.noreply.github.com>
2022-01-26 13:08:35 +0000

    Add project files.

---
+++ CoffeeLover/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CoffeeLover.Models;
+
+namespace CoffeeLover.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CoffeeLover.Models.Address> Address { get; set; }
+        public DbSet<CoffeeLover.Models.Cashier> Cashier { get; set; }
+    }
+}
